8355b222c55d1175326e1fe75a32471117fa002f
Danielle Silva <danielle.luisasilva@gmail.com>
2022-05-28 23:28:33 -0300

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+      <div className="header-logo">LOGO</div>
+      <div className="header-nav">
+        <ul className="header-nav-list">
+          <li className="header-nav-item">Home</li>
+          <li className="header-nav-item">About</li>
+          <li className="header-nav-item">Contact</li>
+        </ul>
+      </div>
+    </header>
+  )
+}
---
+++ src/page/Elo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Elo() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
